65327b6f07b1510c3a86463ede7bce01e5e09472
jiashuaijie <35450295+jiashuaijie@users.noreply.github.com>
2020-03-26 15:14:15 +0800

    Add files via upload
    此代码摘抄自王宇斌，其中加入了，绿化功能，在房子之间加入了牡丹花，另外，在房子后面加入了瀑布。最顶层花纹改为了visit my house。

---
+++ students/贾帅杰/welcomehome.py
@@ -0,0 +1,88 @@
+import mcpi.minecraft as minecraft
+import mcpi.block as block
+
+mc = minecraft.Minecraft.create()
+
+pos = mc.player.getTilePos()
+#流动、掉落的方块
+ban=[6,8,9,10,11,12,13,18,26,27]
+global j
+j=0
+
+def shoufig(xo, yo, zo, L, W, H, num):
+    if num >= 0 and num <= 27:
+        fname = "huawen" + str(num) + ".csv"
+        f = open(fname, "r")
+        offsetX = 4
+        offsetZ = 0
+        for line in f.readlines():
+            data = line.split(",")
+            for cell in data:
+                if cell == "1" or cell == "1\n":
+                    mc.setBlock(xo + offsetX, yo + H - 1, zo + offsetZ, 35, 1)
+                offsetZ = offsetZ + 1
+            offsetX = offsetX - 1
+            offsetZ = 0
+
+
+def build(xo, yo, zo, L, W, H, M, fig):
+    global j
+    for i in ban:
+        if i == M:
+            M = 169 
+#墙
+    for y in range(H):
+        for x in range(L):
+            mc.setBlock(xo + x, yo + y, zo, M)
+            mc.setBlock(xo + x, yo + y, zo + W - 1, M)
+        for z in range(W - 2):
+            mc.setBlock(xo, yo + y, zo + 1 + z, M)
+            mc.setBlock(xo + L - 1, yo + y, zo + 1 + z, M)
+#屋顶地板
+    for x in range(L):
+        for z in range(W):
+            mc.setBlock(xo + x, yo, zo + z, M)
+            mc.setBlock(xo + x, yo + H - 1, zo + z, 35, 0)
+#绿化修饰
+    for x in range(10,20) :
+        for z in range(30) :
+            mc.setBlock(xo + x, yo, zo + z, M)
+            if z%2 == 0 :
+                mc.setBlock(xo + x, yo, zo + z, block.WOOL.id)
+    for x in range(10) :
+        for z in range(10,30) :
+            mc.setBlock(xo + x, yo, zo + z, M)
+            if z in [15,16]:
+                mc.setBlock(xo + x, yo+1, zo + z, 3)
+                mc.setBlock(xo + x, yo+2, zo + z, 37)
+#照明（海晶灯）
+    for x in range(2):
+        for z in range(2):
+            mc.setBlock(xo + x + (L - 2) // 2, yo,
+                        zo + (W - 2) // 2 + z, 169)
+            mc.setBlock(xo + x + (L - 2) // 2, yo + H - 2,
+                        zo + (W - 2) // 2 + z, 169)
+            mc.setBlock(xo, yo + 4 + x, zo + (W - 2) // 2 + z, 169)
+
+#门
+    mc.setBlock(xo + L / 2, yo + 1, zo, 0)
+    mc.setBlock(xo + L / 2, yo + 2, zo, 0)
+    #玻璃
+    for z in range(2):
+        for y in range(2):
+            mc.setBlock(xo + L - 1, yo + y + 2, zo + (W - 2) / 2 + z,
+                        20)
+
+
+#屋顶花纹
+    shoufig(xo + (L - 5) / 2,yo, zo + (W - 6) / 2,L, W, H,  num=fig)
+
+i=0
+for a in range(3):
+    for b in range(3):
+        for c in range(3):
+            i+=1
+            j=j+1
+            build(pos.x+20*c, pos.y+20*a, pos.z+20*b, 10+2*c, 10+2*b, 10+2*a, i, i)
+for d in range(60):
+    mc.setBlock(pos.x + d, pos.y + 60, pos.z + 73, 8)
