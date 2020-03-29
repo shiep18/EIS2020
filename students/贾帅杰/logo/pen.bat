c9cde9892bb1e79a658a487362449337d2196dea
jiashuaijie <35450295+jiashuaijie@users.noreply.github.com>
2020-03-29 08:31:55 +0800

    Add files via upload

---
+++ students/贾帅杰/logo/pen.bat
@@ -0,0 +1,8 @@
+rem delete any previous 
+del pen.binvox
+
+rem use default resolution of 256 and downsample twice, so resolution is 64
+rem you will to add your own arguments here - see http://www.patrickmin.com/minecraft/
+binvox pen.stl -down -down
+rem now display it
+viewvox pen.binvox
