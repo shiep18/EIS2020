126215f538adbfdb42ebd26e54e4deab15726f25
jiashuaijie <35450295+jiashuaijie@users.noreply.github.com>
2020-03-29 08:26:13 +0800

    Add files via upload

---
+++ students/贾帅杰/logo/mylogo.bat
@@ -0,0 +1,8 @@
+rem delete any previous 
+del mylogo.binvox
+
+rem use default resolution of 256 and downsample twice, so resolution is 64
+rem you will to add your own arguments here - see http://www.patrickmin.com/minecraft/
+binvox mylogo.stl -down -down
+rem now display it
+viewvox mylogo.binvox
