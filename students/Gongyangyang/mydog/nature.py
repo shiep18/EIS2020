f0446538de2fb95851663f623d1f845068e9c910
gohiy <57204793+rinze70@users.noreply.github.com>
2020-03-31 13:59:46 +0800

    Add files via upload

---
+++ students/Gongyangyang/mydog/nature.py
@@ -0,0 +1,87 @@
+import requests
+import json
+
+APIKey='LuUwUnqxmZYd1pCuwmn1WyTL'
+SecretKey='ZBsM5DV0IFAmYAQ1sAvOSYCGpqbgMqi2'
+
+
+#得到请求url
+def get_url():
+    url=0
+    #通过API Key和Secret Key获取access_token
+    AccessToken_url='https://aip.baidubce.com/oauth/2.0/token?grant_type=client_credentials&client_id={}&client_secret={}'.format(APIKey,SecretKey)
+    res = requests.get(AccessToken_url)
+    json_data = json.loads(res.text)
+    #print(json.dumps(json_data, indent=4, ensure_ascii=False))
+    if not json_data or 'access_token' not in json_data:
+        print("获取AccessToken的json数据失败")
+    else:
+        accessToken=json_data['access_token']
+        #将得到的access_token加到请求url中
+        url='https://aip.baidubce.com/rpc/2.0/nlp/v1/lexer?charset=UTF-8&access_token={}'.format(accessToken)
+    # print(url)
+    return url
+
+#创建请求，获取数据
+def get_address(url,text):
+    address=''#存储得到的地址
+    body={
+        "text": text,
+    }
+    body2 = json.dumps(body)#将字典形式的数据转化为字符串,否则报错
+    #创建Header请求
+    header={
+        'content-type': 'application/json'
+    }
+    res = requests.post(url,headers=header,data=body2)# 推荐使用post
+    json_data = json.loads(res.text)
+    if not json_data or 'error_code' in json_data:
+        # print(json.dumps(json_data, indent=4, ensure_ascii=False))
+        print("获取关键词的Json数据失败")
+    else:
+        for item in json_data['items']:
+            if item['ne'] == "LOC" :
+                # print(item['item'])
+                address = item['item']
+        return address
+
+
+def getCity(text):
+    # url = "https://api.shenjian.io/nlp/lexer?appid=07e8d355ba0a44f0d9580b669eb2bfc3&text=" + text
+    res = requests.post(url)
+    info = dict(res.json())
+    info = dict(info)
+    infoItems = info["data"]["items"]
+
+    result = ""
+    for num in range(len(infoItems)):
+        if infoItems[num]["ne"] == "LOC":
+            result = infoItems[num]["item"]
+            break
+        
+    if result == "":
+        return ""
+    else:
+        print()
+        print("城市：%s"%result)
+        return result
+    
+def getMusicName(text):
+    url = "https://api.shenjian.io/nlp/lexer?appid=07e8d355ba0a44f0d9580b669eb2bfc3&text=" + text
+    res = requests.post(url)
+    info = dict(res.json())
+    info = dict(info)
+    infoItems = info["data"]["items"]
+    #print(infoItems)
+    result = ""
+    for num in range(len(infoItems)):
+        if infoItems[num]["item"] == "听" or infoItems[num]["item"] == "想听" or infoItems[num]["item"] == "要听" or infoItems[num]["item"] == "首" or infoItems[num]["item"] == "一首":
+            num += 1
+            for j in range(len(infoItems) - num):
+                result += infoItems[num+j]["item"]
+            break
+    print()
+    return result   
+            
+
+#print(getMusicName(""))
---
+++ students/Gongyangyang/mydog/weather.py
@@ -0,0 +1,39 @@
+import requests
+ 
+ 
+
+def getWeatherInfo(city):
+    
+    key = "3bffba48276c408b9107e275a51f111e"
+    url = "https://free-api.heweather.net/s6/weather?location=" + city + "&key=" + key
+    res = requests.post(url)
+    info = dict(res.json())
+    info = dict(info)
+    try:
+        nowInfo = info["HeWeather6"][0]["now"]
+        lifeStyle = info["HeWeather6"][0]["lifestyle"]
+        location = info["HeWeather6"][0]["basic"]["location"]
+        parentCity = info["HeWeather6"][0]["basic"]["parent_city"]
+
+        #print(nowInfo)
+        #print(lifeStyle)
+        #print(location)
+        tmp = nowInfo["tmp"]
+        fl = nowInfo["fl"]
+        cond = nowInfo["cond_txt"]
+        windDir = nowInfo["wind_dir"]
+        windSc = nowInfo["wind_sc"]
+        hum = nowInfo["hum"]
+        vis = nowInfo["vis"]
+        comf = lifeStyle[0]["txt"]
+        drsg = lifeStyle[1]["txt"]
+        flu = lifeStyle[2]["txt"]
+        airBrf = lifeStyle[7]["brf"]
+        cw = lifeStyle[6]["txt"]
+
+        return ("今天%s的天气状况：%s，温度：%s℃，体感温度：%s℃，空气质量：%s，风向：%s，风力：%s级，湿度：百分之%s，能见度：%s公里，%s%s%s"%(location,cond,tmp,fl,airBrf,windDir,windSc,hum,vis,flu,drsg,cw)),location,tmp
+    except:
+        return("没有查到%s的天气"%city)
+
+
+
