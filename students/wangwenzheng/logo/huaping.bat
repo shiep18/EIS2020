37fb2dd06abc745fc8ba7e1fdae1f495435ec579
luqi-123 <62538351+luqi-123@users.noreply.github.com>
2020-03-31 22:01:01 +0800

    Add files via upload

---
+++ students/wangwenzheng/logo/huaping.bat
@@ -0,0 +1,8 @@
+rem delete any previous 
+del huaping.binvox
+
+rem use default resolution of 256 and downsample twice, so resolution is 64
+rem you will to add your own arguments here - see http://www.patrickmin.com/minecraft/
+binvox huaping.stl -down -down -down
+rem now display it
+viewvox huaping.binvox
