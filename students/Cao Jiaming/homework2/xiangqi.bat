00f60086953e4378d96e5176fe9fddb2611c7233
CaoJiaMing-666 <62164524+CaoJiaMing-666@users.noreply.github.com>
2020-04-01 17:03:23 +0800

    Create xiangqi.bat

---
+++ students/Cao Jiaming/homework2/xiangqi.bat
@@ -0,0 +1,16 @@
+
+rem delete any previous 
+
+del xiangqi.binvox
+
+
+
+rem use default resolution of 256 and downsample twice, so resolution is 64
+
+rem you will to add your own arguments here - see http://www.patrickmin.com/minecraft/
+
+binvox xiangqi.stl -down -down -down
+
+rem now display it
+
+viewvox xiangqi.binvox
