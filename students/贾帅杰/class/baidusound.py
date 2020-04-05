73ab530103753dda6832a737192977bcdfeebb26
jiashuaijie <35450295+jiashuaijie@users.noreply.github.com>
2020-04-05 10:57:30 +0800

    Add files via upload

---
+++ students/贾帅杰/class/baidusound.py
@@ -0,0 +1,19 @@
+from aip import AipSpeech
+import os
+from mutagen.mp3 import MP3
+
+
+def speak(yuyin):
+    APP_ID='17980615'
+    API_KEY='pIQlVZYLO4aPDLM55GGMGe5L'
+    SECRET_KEY='0Pba78PqkEch32ozc4IbvMFmjN7kaejw'
+
+    client=AipSpeech(APP_ID,API_KEY,SECRET_KEY)
+    voice=client.synthesis(yuyin,'zh',6,{'vol':15,'per':3,'spd':5})
+    with open("playback.mp3",'wb') as fp:
+        fp.write(voice)
+
+    audio=MP3('playback.mp3')
+    time=int(audio.info.length)
+    os.system("playback.mp3")
+    return time
---
+++ students/贾帅杰/class/videos1.py
@@ -0,0 +1,53 @@
+import cv2
+import numpy as np
+import recordsound as rs
+import wave2pinyin as w2p
+import baidusound as bd
+import time
+cap = cv2.VideoCapture(0) 
+i=0
+j=0
+background = None
+  
+while True:
+  # 读取视频流
+  ret, frame= cap.read()
+  gray_lwpCV = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+ 
+  # 将第一帧设置为整个输入的背景
+  if background is None:
+      background = gray_lwpCV
+      continue
+  # 对于每个从背景之后读取的帧都会计算其与背景之间的差异，并得到一个差分图（different map）。
+  diff = cv2.absdiff(background, gray_lwpCV)
+  
+  diff = cv2.threshold(diff, 25, 255, cv2.THRESH_BINARY)[1] # 二值化阈值处理
+  
+  # 显示矩形框
+  contours, hierarchy = cv2.findContours(diff.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE) # 该函数计算一幅图像中目标的轮廓
+  for c in contours:
+    if cv2.contourArea(c) < 1500: # 对于矩形区域，只显示大于给定阈值的轮廓，所以一些微小的变化不会显示。对于光照不变和噪声低的摄像头可不设定轮廓最小尺寸的阈值
+      continue
+    i=i+1
+    if i>1000:
+      i=2
+    (x, y, w, h) = cv2.boundingRect(c) # 该函数计算矩形的边界框
+    cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+    
+  cv2.imshow('contours', frame)
+  cv2.imshow('dis', diff)
+  if j>30:
+    str1='你好'
+    times=bd.speak(str1)
+    time.sleep(2)
+    j=0
+  if i>0:
+    j=j+1
+  i=0
+  key = cv2.waitKey(1) & 0xFF
+  # 按'q'健退出循环
+  if key == ord('q'):
+    break
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
