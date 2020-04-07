639c4470fecc5ec5398a41d21b737894b16c4c10
lvtaosam <62537539+lvtaosam@users.noreply.github.com>
2020-04-07 15:05:16 +0800

    Add files via upload

---
+++ students/LvTao/20200330/move.py
@@ -0,0 +1,33 @@
+import mcpi.minecraft as minecraft
+import mcpi.block as block
+import csv
+from time import *
+
+mc = minecraft.Minecraft.create()
+pos = mc.player.getTilePos()
+
+def move(dir):
+    if dir == "left":
+        for i in range(10):
+            mc.player.setPos(pos.x+i,pos.y,pos.z)
+            sleep(0.1)
+    elif dir == "right":
+        for i in range(10):
+            mc.player.setPos(pos.x-i,pos.y,pos.z)
+            sleep(0.1)
+    elif dir == "front":
+        for i in range(10):
+            mc.player.setPos(pos.x,pos.y,pos.z+i)
+            sleep(0.1)
+    elif dir == "back":
+        for i in range(10):
+            mc.player.setPos(pos.x,pos.y,pos.z-i)
+            sleep(0.1)
+    elif dir == "up":
+        for i in range(10):
+            mc.player.setPos(pos.x,pos.y+i,pos.z)
+            sleep(0.1)
+    else :
+        for i in range(10):
+            mc.player.setPos(pos.x,pos.y-i,pos.z)
+            sleep(0.1)
