38a45a2857c1dec0656c7672b828351eff7681bf
Guochanghao324 <62537595+Guochanghao324@users.noreply.github.com>
2020-04-16 09:40:57 +0800

    Add files via upload

---
+++ students/Guochanghao/myled/myled.py
@@ -0,0 +1,56 @@
+import mcpi.minecraft as minecraft
+import mcpi.block as block
+mc = minecraft.Minecraft.create()
+pos = mc.player.getTilePos()
+import csv
+import serial
+import serial.tools.list_ports
+import os
+import time
+import random
+ser=serial.Serial(port='COM2')
+time.sleep(2)
+class House:
+    def __init__(self,x,y,z,l,w,h,num):
+        self.x=x
+        self.y=y
+        self.z=z
+        self.l=l
+        self.w=w
+        self.h=h
+        self.num=num
+
+    def house(self):
+        material = ['1','17','5','41','45','57','80','91','155','133']#10
+        ran = random.randint(0,9)
+        mc.setBlocks(self.x,self.y-1,self.z,self.x+self.l,self.y+self.h-1,self.z+self.w,int(material[ran]))
+        mc.setBlocks(self.x+1,self.y,self.z+1,self.x+self.l-1,self.y+self.h-2,self.z+self.w-1,0)   #建一个火柴盒
+        mc.setBlocks(self.x+3,self.y+1,self.z,self.x+4,self.y+2,self.z,block.GLASS.id)#窗
+        mc.setBlocks(self.x+6,self.y,self.z,self.x+6,self.y+1,self.z,0)              
+        for i in range(10):
+              for j in range(10):
+                     roof = random.randint(0,27)
+                     mc.setBlock(self.x+i,self.y+5,self.z+j,35,roof)#花屋顶
+
+    def isInHouse(self,x0,y0,z0):
+        if self.x<=x0<=self.x+self.l and self.y<=y0<=self.y+self.h and self.z<=z0<=self.z+self.w:
+            return True
+        else:
+            return False
+
+reader = csv.reader(open('new.csv'))
+data=[]
+for r in reader:
+
+    data.append(r)
+for i in range(27):
+    for j in range(7):
+        data[i][j+1]=int(data[i][j+1])
+    data[i][0]=House(data[i][1],data[i][2],data[i][3],data[i][4],data[i][5],data[i][6],data[i][7])
+    data[i][0].house()
+
+while True:
+    pos = mc.player.getTilePos()#获取人物坐标            
+    for i in range(27):
+        if data[i][0].isInHouse(pos.x,pos.y,pos.z):
+            print("您现在处于房间内")
