9af34859314a05ddcdce0851f886fd2c9c1e7a2b
caojiayang <62537846+caojiayang@users.noreply.github.com>
2020-04-18 15:51:02 +0800

    Update socket.py

--- students/CaoJiaYang/socket&box/socket.py
+++ students/CaoJiaYang/socket&box/socket.py
@@ -1,1 +1,10 @@
-
+import socket
+s = socket.socket()
+s.connect(('127.0.0.1',4711))
+print("connected")
+msg = "player.getPos()\n"  
+s.send(msg.encode("ascii"))
+x,y,z = map(int,eval(s.recv(1024)))
+msg = f"world.setBlocks({x},{y},{z},{x+2},{y},{z+2},41)\r"
+s.send(msg.encode("ascii"))
+s.close()
