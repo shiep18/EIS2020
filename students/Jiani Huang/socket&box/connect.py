2ed888f2382e94db92f6975574dbfd457f6d5009
Lin1127 <62537436+Lin1127@users.noreply.github.com>
2020-04-18 08:48:56 +0800

    Update connect.py

--- students/Jiani Huang/socket&box/connect.py
+++ students/Jiani Huang/socket&box/connect.py
@@ -3,11 +3,11 @@ import socket
 s=socket.socket()
 s.connect(("127.0.0.1",4711))
 print("connected")
-##msg=input(">")
-##s.send(msg.encode("ascii"))
 
-msg1 = "player.setPos(120,2,-95)\r"
-msg2 = "world.setBlocks(119,1,-94,121,1,-96,41)\r"
-
-s.send(msg1.encode("ascii"))
-s.send(msg2.encode("ascii"))
+msg = "player.getPos()\r"
+s.send(msg.encode("ascii"))
+x,y,z = map(int,eval(s.recv(1024)))
+#print(x,y,z)
+msg = f"world.setBlocks({x-1},{y-1},{z-1},{x+1},{y-1},{z+1},41)\r"
+s.send(msg.encode("ascii"))
+s.close()
