75307c0cfc8ab7117df40475dca07542862d678e
Wingplane <zy6124@126.com>
2020-05-08 01:27:30 +0800

    Add files via upload

---
+++ students/zhangyue/homework9/socket.py
@@ -0,0 +1,12 @@
+import socket
+
+s = socket.socket()
+s.connect(("127.0.0.1", 4711))
+print("connected")
+
+msg = "player.getPos()\r"
+s.send(msg.encode("ascii"))
+x, y, z = map(int, eval(s.recv(1024)))
+msg = f"world.setBlocks({x - 1},{y - 1},{z - 1},{x + 1},{y - 1},{z + 1},41)\r"
+s.send(msg.encode("ascii"))
+s.close()
