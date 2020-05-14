e0bb7a2f61d6983c68d04892173f8f9d919a3514
caojiayang <62537846+caojiayang@users.noreply.github.com>
2020-05-14 23:16:03 +0800

    Add files via upload

---
+++ students/CaoJiaYang/login&vote/login(android)/Main2Activity.java
@@ -0,0 +1,27 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import static com.example.myapplication.R.*;
+
+public class Main2Activity extends AppCompatActivity {
+    private Button login;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        login=findViewById(R.id.button3);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent =new Intent(Main2Activity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
