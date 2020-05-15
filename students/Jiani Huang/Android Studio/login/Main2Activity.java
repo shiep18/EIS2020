1ff69e62122f14d13505bf7b2f545b49c34d3219
Lin1127 <62537436+Lin1127@users.noreply.github.com>
2020-05-15 10:24:30 +0800

    Add files via upload

---
+++ students/Jiani Huang/Android Studio/login/Main2Activity.java
@@ -0,0 +1,25 @@
+package com.example.log;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Main2Activity extends AppCompatActivity {
+    private Button back;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        back = findViewById(R.id.back);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Main2Activity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
