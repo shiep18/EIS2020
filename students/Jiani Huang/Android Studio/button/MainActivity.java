cc9616222f468b3cbd17e9a79cdb731abb038ff9
Lin1127 <62537436+Lin1127@users.noreply.github.com>
2020-05-15 10:25:35 +0800

    Add files via upload

---
+++ students/Jiani Huang/Android Studio/button/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+private Button button;
+private TextView textView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button);
+        textView = findViewById(R.id.textView);
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textView.setText("hello，已触发！");
+            }
+        });
+    }
+}
