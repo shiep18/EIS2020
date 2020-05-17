18bd6afea957fd70b6eabee6df446344bc1347f9
zhangyue0529 <zy6124@126.com>
2020-05-17 12:10:01 +0800

    zhangyue update
    zhangyue update

--- students/zhangyue/homework16/mysite/mysite/urls.py
+++ students/zhangyue/homework16/mysite/mysite/urls.py
@@ -18,5 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('polls/', include('polls.urls')),
+    path('topics/', include('topics.urls')),
     path('admin/', admin.site.urls),
 ]
---
+++ students/zhangyue/homework16/mysite/topics/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+app_name = 'polls'
+urlpatterns = [
+    path('', views.index, name='index'),
+]
