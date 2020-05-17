3b4dabf0eb9f78d82759a489d4ae721058051321
caojiayang <62537846+caojiayang@users.noreply.github.com>
2020-05-17 15:47:37 +0800

    Update baidu.py

--- students/CaoJiaYang/PyAutoGUI_5.15/baidu.py
+++ students/CaoJiaYang/PyAutoGUI_5.15/baidu.py
@@ -1,1 +1,14 @@
+import pyautogui
+import time
+from selenium import webdriver
 
+driver = webdriver.Chrome()
+driver.maximize_window()  # 窗口最大化显示
+
+#driver.get("https://www.taobao.com")
+driver.get('http://www.baidu.com')
+#s=driver.find_element_by_xpath('//*[@id="db-global-nav"]/div/div[4]/ul/li[3]/a').click()
+s=driver.find_element_by_xpath('//*[@id="kw"]')
+
+s.send_keys('曹嘉扬 上海电力大学')
+s=driver.find_element_by_xpath('//*[@id="su"]').click()
