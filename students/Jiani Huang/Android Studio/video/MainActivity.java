39322bc4fb9c74161b5c0d256e292365deee620a
Lin1127 <62537436+Lin1127@users.noreply.github.com>
2020-05-20 15:26:57 +0800

    Add files via upload

---
+++ students/Jiani Huang/Android Studio/video/MainActivity.java
@@ -0,0 +1,82 @@
+package com.example.video;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+public class MainActivity extends AppCompatActivity {
+    private VideoView videoView;
+
+    private Button btn_start,btn_end,button2,button3;
+    private MediaController mediaController;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView(){
+        videoView = (VideoView) findViewById(R.id.videoView);
+        btn_start = (Button) findViewById(R.id.btn_start);
+        btn_end = (Button) findViewById(R.id.btn_end);
+
+        button2 = (Button) findViewById(R.id.button2);
+        button3 = (Button) findViewById(R.id.button3);
+        btn_start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                init();
+            }
+        });
+        btn_end.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                videoView.stopPlayback();
+            }
+        });
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                init1();
+            }
+        });
+        button3.setOnClickListener(new View.OnClickListener() {
+            Intent intent_video = new Intent(MainActivity.this,login.class);
+
+            @Override
+            public void onClick(View v) {
+                startActivity(intent_video);
+            }
+        });
+    }
+    private void init(){
+        videoView = (VideoView) findViewById(R.id.videoView);
+        mediaController = new MediaController(this);
+        String uri = "android.resource://"+getPackageName()+"/"+R.raw.lego;
+        videoView.setVideoURI(Uri.parse(uri));
+        videoView.setMediaController(mediaController);
+        mediaController.setMediaPlayer(videoView);
+        videoView.requestFocus();
+        videoView.start();
+
+    }
+    private void init1() {
+        videoView = (VideoView) findViewById(R.id.videoView);
+        mediaController = new MediaController(this);
+        String uri = "android.resource://" + getPackageName() + "/" + R.raw.hear;
+        videoView.setVideoURI(Uri.parse(uri));
+        videoView.setMediaController(mediaController);
+        mediaController.setMediaPlayer(videoView);
+        videoView.requestFocus();
+        videoView.start();
+
+    }
+}
