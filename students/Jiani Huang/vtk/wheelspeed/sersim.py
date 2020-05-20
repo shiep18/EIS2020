8eb133de4727568c55c986effdfc7acaf1a39f65
Lin1127 <62537436+Lin1127@users.noreply.github.com>
2020-05-20 12:20:58 +0800

    Add files via upload

---
+++ students/Jiani Huang/vtk/wheelspeed/sersim.py
@@ -0,0 +1,10 @@
+import serial
+
+ser = serial.Serial("COM2")
+#ser.write("100,100,100\n".encode())
+while True:
+    c = input("please input speed")
+    print(c)
+    if c == "q":
+        break
+    ser.write(c.encode())
