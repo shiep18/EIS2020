55ed811b6e4f4394ec80dbe18a992f4864415621
liruomeng <62537623+liruomeng@users.noreply.github.com>
2020-05-22 00:56:21 +0800

    Add files via upload

---
+++ students/LiRuomeng/5.18/myvtkcone.py
@@ -0,0 +1,48 @@
+import vtk
+import time
+
+cone = vtk.vtkConeSource()
+
+mapper = vtk.vtkPolyDataMapper()
+mapper.SetInputConnection(cone.GetOutputPort())
+
+actor = vtk.vtkActor()
+actor.SetMapper(mapper)
+
+filename = "wheel1.stl"
+ 
+reader = vtk.vtkSTLReader()
+reader.SetFileName(filename)
+ 
+mapper1 = vtk.vtkPolyDataMapper()
+mapper1.SetInputConnection(reader.GetOutputPort())
+
+actor1 = vtk.vtkActor()
+actor1.SetMapper(mapper1)
+
+window = vtk.vtkRenderWindow() # Sets the pixel width, length of the window.
+window.SetSize(500, 500)
+
+interactor = vtk.vtkRenderWindowInteractor()
+interactor.SetRenderWindow(window)
+
+renderer = vtk.vtkRenderer()
+window.AddRenderer(renderer)
+
+renderer.AddActor(actor)
+renderer.AddActor(actor1)
+renderer.SetBackground(0, 0, 1)#背景颜色
+
+actor.SetOrigin(0.5,0,0)
+
+actor1.SetOrigin(-4,7,10)
+actor1.SetPosition(0,5,0)
+
+
+for i in range(36):
+    time.sleep(1)
+    actor.RotateY(10)
+    actor1.RotateY(10)
+    window.Render()
+
+
