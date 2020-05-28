309cd36b1db000f12b825e742d97f54b37995f41
20171583沈嘉成 <62537536+Shen1231@users.noreply.github.com>
2020-05-28 20:34:03 +0800

    Add files via upload

---
+++ students/ShenJiaCheng/20200525/hachina3.py
@@ -0,0 +1,32 @@
+# 引入记录日志的库
+import logging
+ 
+DOMAIN = "hachina3"
+ENTITYID = DOMAIN + ".hello_world"
+ 
+# 在python中，__name__代表模块名字
+_LOGGER = logging.getLogger(__name__)
+ 
+ 
+def setup(hass, config):
+    """配置文件加载后，setup被系统调用."""
+    attr = {"icon": "mdi:yin-yang",
+            "friendly_name": "欢迎来到电大",
+            "slogon": "积木构建智慧空间！", }
+    hass.states.set(ENTITYID, 'OFF', attributes=attr)
+ 
+    def change_state(call):
+        """change_state函数切换改变实体的状态."""
+        # 记录info级别的日志
+        _LOGGER.info("hachina's change_state service is called.")
+ 
+        # 切换改变状态值
+        if hass.states.get(ENTITYID).state == 'OFF':
+            hass.states.set(ENTITYID, 'ON', attributes=attr)
+        else:
+            hass.states.set(ENTITYID, 'OFF', attributes=attr)
+ 
+    # 注册服务hachina.change_state
+    hass.services.register(DOMAIN, 'change_state', change_state)
+ 
+    return True
