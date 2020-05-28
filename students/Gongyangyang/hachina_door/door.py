e1c0422a4f31e6132b6f04e56b8da56d3121f028
gohiy <57204793+rinze70@users.noreply.github.com>
2020-05-28 22:04:20 +0800

    Add files via upload

---
+++ students/Gongyangyang/hachina_door/door.py
@@ -0,0 +1,39 @@
+import cv2
+
+filepath = "C://Apache24//htdocs//door.html"
+on = cv2.imread("./door_states/door_on.png")
+off = cv2.imread("./door_states/door_off.png")
+locked = cv2.imread("./door_states/door_locked.png")
+
+rehead = '<meta http-equiv="refresh" content="1">\n'
+
+cv2.imshow('result',on)
+
+def click(event,x,y,flags,param):
+    if event==cv2.EVENT_LBUTTONDOWN:
+        print('mouse coords:',x,y)        
+        if 316<x<480 and 52<y<480:
+            print ('pd')
+            #p=T
+            cv2.imshow('result',on)
+            with open(filepath, "w") as f:
+                f.write(rehead + "on")
+            #pass
+        elif 250<x<295 and 230<y<278:   
+            cv2.imshow('result',locked)
+            with open(filepath, "w") as f:
+                f.write(rehead + "locked")
+            cv2.setMouseCallback('result',click)
+
+        else:
+            cv2.imshow('result',off)
+            with open(filepath, "w") as f:
+                f.write(rehead + "off")
+            cv2.setMouseCallback('result',click)
+            
+cv2.namedWindow('result')
+cv2.setMouseCallback('result',click)
+while True:
+    if cv2.waitKey(10)&0xFF==27:
+            break
+cv2.destroyAllWindows()
