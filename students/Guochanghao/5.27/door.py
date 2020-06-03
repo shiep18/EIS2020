91764f9a953052f6c734f867800001a44adef411
Guochanghao324 <62537595+Guochanghao324@users.noreply.github.com>
2020-06-03 18:11:55 +0800

    Add files via upload

---
+++ students/Guochanghao/5.27/door.py
@@ -0,0 +1,33 @@
+import cv2
+
+pd = cv2.imread('close.jpg')
+pd1 = cv2.imread('open.jpg')
+pd2 = cv2.imread('lock.jpg')
+
+cv2.imshow('result',pd)
+
+def click(event,x,y,flags,param):
+    if event==cv2.EVENT_LBUTTONDOWN:
+        print('mouse coords:',x,y)    
+        if 175<x<225 and 300<y<350:
+            print ('open')
+            cv2.imshow('result',pd1)
+            f=open("C:\\Apache24\\htdocs\\index.html", "w")
+            f.write("<meta http-equiv=\"refresh\" content=\"1\">\n")
+            f.write("open")
+        elif 340<x<390 and 275<y<325:
+            cv2.imshow('result',pd2)
+            f=open("C:\\Apache24\\htdocs\\index.html", "w") 
+            f.write("<meta http-equiv=\"refresh\" content=\"1\">\n")
+            f.write("lock")
+        else :
+            cv2.imshow('result',pd)
+            f=open("C:\\Apache24\\htdocs\\index.html", "w") 
+            f.write("<meta http-equiv=\"refresh\" content=\"1\">\n")
+            f.write("close")
+cv2.namedWindow('result')
+cv2.setMouseCallback('result',click)
+while True:
+    if cv2.waitKey(10)&0xFF==27:
+            break
+cv2.destroyAllWindows()
---
+++ students/Guochanghao/5.27/light.py
@@ -0,0 +1,29 @@
+import cv2
+pd = cv2.imread('1.jpg')
+pd1 = cv2.imread('2.jpg')
+global p
+F=0
+T=1
+p=F
+cv2.imshow('result',pd)
+def click(event,x,y,flags,param):
+    if event==cv2.EVENT_LBUTTONDOWN:
+        print('mouse coords:',x,y)        
+        if 135<x<295 and 90<y<290:
+            print ('pd')
+            #p=T
+            cv2.imshow('result',pd)
+            f = open("C:\\Apache24\\htdocs\\index.html",'w')
+            f.write("on")
+            #pass
+        else:
+            cv2.imshow('result',pd1)
+            cv2.setMouseCallback('result',click)
+            f = open("C:\\Apache24\\htdocs\\index.html",'w')
+            f.write("off")
+cv2.namedWindow('result')
+cv2.setMouseCallback('result',click)
+while True:
+    if cv2.waitKey(10)&0xFF==27:
+        break
+cv2.destroyAllWindows()
