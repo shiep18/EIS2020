b1dce21c36dcc652797d196a4e40c33ed4821a4e
qqqadsdfd <53887938+qqqadsdfd@users.noreply.github.com>
2020-06-08 10:14:44 +0800

    Add files via upload

---
+++ team/team1/test/窗口2.py
@@ -0,0 +1,158 @@
+import tkinter as tk
+import tkinter.messagebox
+import pickle
+import pymysql
+import cv2
+import os
+import My_Speech_end
+import time
+
+window = tk.Tk()
+window.title('')
+w = window.winfo_screenwidth()
+h = window.winfo_screenheight()
+window.geometry("%dx%d" %(w, h))
+
+def usr_sign_up():
+
+    window_sign_up = tk.Toplevel(window)
+    h = window_sign_up.winfo_screenwidth()
+    w = window_sign_up.winfo_screenheight()
+    window_sign_up.geometry('%dx%d' %(h, w))
+    window_sign_up.title('借球')
+
+    cap = cv2.VideoCapture(0)
+    conn = pymysql.connect('localhost', 'root', '', 'basketball')
+    cursor = conn.cursor()
+
+    tk.Label(window_sign_up, text='年级', font=('Arial', 14)).place(x=10, y=90)
+    tk.Label(window_sign_up, text='班级', font=('Arial', 14)).place(x=10, y=130)
+    tk.Label(window_sign_up, text='学号', font=('Arial', 14)).place(x=10, y=170)
+    tk.Label(window_sign_up, text='姓名', font=('Arial', 14)).place(x=10, y=210)
+    tk.Label(window_sign_up, text='数量(借球)', font=('Arial', 14)).place(x=10, y=250)
+
+    var_usr_name = tk.StringVar()
+    entry_usr_name = tk.Entry(window_sign_up, textvariable=var_usr_name, font=('Arial', 14))
+    entry_usr_name.place(x=120,y=90)
+
+    var_usr_name = tk.StringVar()
+    entry_usr_name1 = tk.Entry(window_sign_up, textvariable=var_usr_name, font=('Arial', 14))
+    entry_usr_name1.place(x=120,y=130)
+
+    var_usr_name = tk.StringVar()
+    entry_usr_name2 = tk.Entry(window_sign_up, textvariable=var_usr_name, font=('Arial', 14))
+    entry_usr_name2.place(x=120,y=170)
+
+    var_usr_name = tk.StringVar()
+    entry_usr_name3 = tk.Entry(window_sign_up, textvariable=var_usr_name, font=('Arial', 14))
+    entry_usr_name3.place(x=120,y=210)
+
+    var_usr_name = tk.StringVar()
+    entry_usr_name4 = tk.Label(window_sign_up, text='0', font=('Arial', 14))
+    entry_usr_name4.place(x=120,y=250)
+
+    My_Speech_end.intelluct()
+    borrow = My_Speech_end.sqlname_backnumber('wzy',1,None,'borrow')
+    #rest = My_Speech_end.sqlname_backnumber('wzy', 1, None, 'rest')
+    entry_usr_name4.config(text = '%s'%borrow)
+    def usr_login():
+
+        a=entry_usr_name.get()
+        b=entry_usr_name1.get()
+        c=entry_usr_name2.get()
+        d=entry_usr_name3.get()
+        e=entry_usr_name4.cget('text')
+        f=a + "-" + b + "-" + c + "-" + d + "-" + e + ".jpg"
+        g=a + "," + b + "," + c + ",'" + d + "'," + e
+        #print(type(g))
+        while True:
+            ret, frame = cap.read()
+            cv2.imshow("frame",frame)
+            if cv2.waitKey(1) & 0xFF == 27:
+                cv2.imencode('.jpg',frame)[1].tofile(f)
+                break
+                #os.exit()
+        fp = open(f, 'rb')
+        img = fp.read()
+        fp.close()
+
+        sql = "INSERT INTO basketball (`grade`, `class`, `number`, `name`, `borrow_count`,`pic`) VALUES  ( %s,%s,%s,%s,%s,%s );"
+        args = (a,b,c,d,e,img)
+        try:
+            cursor.execute(sql,args)
+            conn.commit()
+            print("new1")
+        except pymysql.Error as e:
+            print(e.args[0], e.args[1])
+            conn.rollback()
+            conn.close()
+        cap.release()
+        cv2.destroyAllWindows()
+        
+        window_sign_up.destroy()
+        
+    login = tk.Button(window_sign_up, text='确定', command=usr_login)
+    login.place(x=200, y=300)
+
+def sign_up():
+    sign_up = tk.Toplevel(window)
+    h = sign_up.winfo_screenwidth()
+    w = sign_up.winfo_screenheight()
+    sign_up.geometry('%dx%d' %(h, w))
+    sign_up.title('还球')
+
+    conn = pymysql.connect('localhost', 'root', '', 'basketball')
+    cursor = conn.cursor()
+
+    tk.Label(sign_up, text='年级', font=('Arial', 14)).place(x=10, y=90)
+    tk.Label(sign_up, text='班级', font=('Arial', 14)).place(x=10, y=130)
+    tk.Label(sign_up, text='学号', font=('Arial', 14)).place(x=10, y=170)
+    tk.Label(sign_up, text='姓名', font=('Arial', 14)).place(x=10, y=210)
+    tk.Label(sign_up, text='数量(球)', font=('Arial', 14)).place(x=10, y=250)
+
+    var_usr_name = tk.StringVar()
+    entry_usr_name = tk.Entry(sign_up, textvariable=var_usr_name, font=('Arial', 14))
+    entry_usr_name.place(x=120,y=90)
+
+    var_usr_name = tk.StringVar()
+    entry_usr_name1 = tk.Entry(sign_up, textvariable=var_usr_name, font=('Arial', 14))
+    entry_usr_name1.place(x=120,y=130)
+
+    var_usr_name = tk.StringVar()
+    entry_usr_name2 = tk.Entry(sign_up, textvariable=var_usr_name, font=('Arial', 14))
+    entry_usr_name2.place(x=120,y=170)
+
+    var_usr_name = tk.StringVar()
+    entry_usr_name3 = tk.Entry(sign_up, textvariable=var_usr_name, font=('Arial', 14))
+    entry_usr_name3.place(x=120,y=210)
+
+    var_usr_name = tk.StringVar()
+    entry_usr_name4 = tk.Entry(sign_up, textvariable=var_usr_name, font=('Arial', 14))
+    entry_usr_name4.place(x=120,y=250)
+
+    def usr_login():
+        a=entry_usr_name.get()
+        b=entry_usr_name1.get() 
+        c=entry_usr_name2.get()
+        d=entry_usr_name3.get()
+        e=entry_usr_name4.get()
+        sql = 'DELETE FROM `basketball` WHERE grade == '+ a +' AND class == '+ b +' AND number == '+ c +' AND name == '+ d +' AND borrow_count == '+ e +''
+        try:
+            cursor.execute(sql)
+            conn.commit()
+        except:
+            conn.rollback()
+            conn.close()
+        sign_up.destroy()
+
+    login = tk.Button(sign_up, text='确定', command=usr_login)
+    login.place(x=200, y=300)
+
+btn_login = tk.Button(window, text='借球', command=usr_sign_up)
+btn_login.place(x=100, y=100)
+
+btn_login = tk.Button(window, text='还球', command=sign_up)
+btn_login.place(x=200, y=100)
+
+window.mainloop()
+       
